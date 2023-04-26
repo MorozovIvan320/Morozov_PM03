5b1664ba233b049a275e4855f9908daf1b862510
MorozovIvan320 <96531862+MorozovIvan320@users.noreply.github.com>
2023-04-26 11:55:06 +0300

    first code commit

---
+++ src/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+public class Smartphone
+{
+    public string model;
+    public int price;
+    public int diagonal;
+
+    public class Program
+    {
+
+    }
+
+    public Smartphone(string model, int price, int diagonal)
+    {
+        this.model = model;
+        this.price = price;
+        this.diagonal = diagonal;
+    }
+
+    public string ToString()
+    {
+        return string.Format("Модель: {0}  Цена: {1} Размер диагонали: {2}", model, price, diagonal);
+    }
+}
+
+public class GadgetFactory
+{
+    int cntPhones;
+    public Smartphone[] Phones;
+
+    public GadgetFactory(int cntPhones)
+    {
+        this.cntPhones = cntPhones;
+        Phones = new Smartphone[cntPhones];
+    }
+
+    public void PrintToFile()
+    {
+        using (StreamWriter file = new StreamWriter("final.txt", false, Encoding.UTF8))
+        {
+            foreach (Smartphone compared in this.Phones)
+            {
+                file.WriteLine(compared.ToString());
+            }
+        }
+    }
+}
